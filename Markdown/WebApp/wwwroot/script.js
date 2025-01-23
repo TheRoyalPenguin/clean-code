a9c31d6c2f9094498f2a27b8edd329f880ea4d66
Penguin <amasharafutdinov@kpfu.ru>
2025-01-23 16:24:07 +0300

    Апгрейд фронтенда. Добавил возможность скопировать html, скачать html, развернуть на весь экран окна ввода/вывода и развернуть всё приложение на весь экран .

--- Markdown/WebApp/wwwroot/script.js
+++ Markdown/WebApp/wwwroot/script.js
@@ -1,6 +1,16 @@
 const inputField = document.getElementById('inputField');
 const outputField = document.getElementById('outputField');
 
+const inputContainer = document.getElementById('inputContainer');
+const outputContainer = document.getElementById('outputContainer');
+
+const copyHtmlButton = document.getElementById('copyHtmlButton');
+const downloadHtmlButton = document.getElementById('downloadHtmlButton');
+const messageField = document.getElementById('messageField');
+const fullscreenInputButton = document.getElementById('fullscreenInputButton');
+const fullscreenOutputButton = document.getElementById('fullscreenOutputButton');
+const openFullscreenButton = document.getElementById('openFullscreenButton');
+
 let requestTimer;
 
 inputField.addEventListener("input", async () => {
@@ -14,6 +24,76 @@ inputField.addEventListener("input", async () => {
             await processMarkdownText(inputText);
         }
     }, 250);
+});
+
+// копирование html кода
+copyHtmlButton.addEventListener('click', () => {
+    const htmlContent = outputField.innerHTML;
+
+    try {
+        const textarea = document.createElement('textarea');
+        textarea.value = htmlContent;
+
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+
+        changeTextTemporarily(messageField, 'Скопировано', 'green', 5000);
+        console.log('Копирование в буфер обмена успешно!');
+    } catch (err) {
+        changeTextTemporarily(messageField, 'Ошибка при копировании', 'red', 5000);
+        console.error('Ошибка при копировании: ', err);
+    }
+});
+
+// загрузка html файла
+downloadHtmlButton.addEventListener('click', () => {
+    const htmlContent = outputField.innerHTML;
+
+    try {
+        const blob = new Blob([htmlContent], { type: 'text/html' }); // массив байт
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'htmlContent.html';
+        a.click();
+
+        URL.revokeObjectURL(url); // удаляем объект URL
+
+        changeTextTemporarily(messageField, 'Успешно загружено', 'green', 5000);
+        console.log('Html страница успешно скачена!');
+    } catch (err) {
+        changeTextTemporarily(messageField, 'Ошибка при скачивании', 'red', 5000);
+        console.error('Ошибка при скачивании: ', err);
+    }
+});
+
+fullscreenInputButton.addEventListener('click', () => {
+    if (inputContainer.classList.contains('fullscreen')) {
+        inputContainer.classList.remove('fullscreen');
+    } else {
+        inputContainer.classList.add('fullscreen');
+    }
+});
+
+fullscreenOutputButton.addEventListener('click', () => {
+    if (outputContainer.classList.contains('fullscreen')) {
+        outputContainer.classList.remove('fullscreen');
+    } else {
+        outputContainer.classList.add('fullscreen');
+    }
+});
+
+openFullscreenButton.addEventListener('click', () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+        openFullscreenButton.textContent = 'Выйти из полноэкранного режима'
+    } else {
+        document.exitFullscreen();
+        openFullscreenButton.textContent = 'Полноэкранный режим'
+    }
 });
 
 async function processMarkdownText(inputText) {
@@ -34,4 +114,17 @@ async function processMarkdownText(inputText) {
     catch {
         console.error(error);
     }
+}
+
+function changeTextTemporarily(element, newText, color, duration) {
+    const originalText = element.textContent;
+    const originalColor = element.style.color;
+
+    element.textContent = newText;
+    element.style.color = color;
+
+    setTimeout(() => {
+        element.textContent = originalText;
+        element.style.color = originalColor;
+    }, duration);
 }
