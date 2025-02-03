9beaad84309e126786d1cdcd5f71bfca1addf6b2
Amal Sharafutdinov <42300062+TheRoyalPenguin@users.noreply.github.com>
2025-02-03 18:57:02 +0300

    маленькая правка (ITokenizer)

--- Markdown/Markdown/BaseClasses/MarkdownConverter.cs
+++ Markdown/Markdown/BaseClasses/MarkdownConverter.cs
@@ -1,10 +1,12 @@
-﻿namespace Markdown.BaseClasses;
+﻿using Markdown.Interfaces;
+
+namespace Markdown.BaseClasses;
 
 public class MarkdownConverter : IMarkdownConverter
 {
     public string ConvertToHtml(string markdownText)
     {
-        Tokenizer tokenizer = new Tokenizer();
+        ITokenizer tokenizer = new Tokenizer();
         string htmlString = tokenizer.Tokenize(markdownText).ToHtml();
         return htmlString;
     }
